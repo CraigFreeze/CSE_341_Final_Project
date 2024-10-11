7c1f751ce6e546e695543e4eed9d5958240517a4
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2024-10-11 12:57:25 -0700

    Initial Set Up

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const mongodb = require('./data/database.js');
+
+const port = 3000;
+
+// Server Start
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(process.env.port || port);
+    console.log(`Connected to DB and listening on ${process.env.port || 3000}`);
+  }
+});
