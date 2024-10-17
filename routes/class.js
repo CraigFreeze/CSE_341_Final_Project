f8f4e907a6d8b615201f833eabb782c6ac970094
JosueSinaca <sinaca56@gmail.com>
2024-10-17 01:43:07 -0600

    Finished GET (all), PUT, and DELETE (ONLY for student and class)

--- routes/class.js
+++ routes/class.js
@@ -4,6 +4,9 @@ const classController = require('../controllers/classController.js')
 
 routes.get('/', classController.getAll())
 routes.get('/:id', classController.getOne())
+routes.get('/subject/:subject', classController.getSubject())
 routes.post('/', classController.createClass)
+routes.put('/:classId', classController.updateClass)
+routes.delete('/:classId', classController.deleteClass)
 
 module.exports = routes;
--- routes/student.js
+++ routes/student.js
@@ -4,6 +4,10 @@ const studentController = require('../controllers/studentController.js')
 
 routes.get('/', studentController.getAll())
 routes.get('/:id', studentController.getOne())
+routes.get('/findByFirstName/:first_name', studentController.findByFirstName())
+routes.get('/findByLastName/:last_name', studentController.findByLastName())
 routes.post('/', studentController.createStudent)
+routes.put('/:studentId', studentController.updateStudent)
+routes.delete('/:studentId', studentController.deleteStudent)
 
 module.exports = routes;
