77e379c22f662eae20234bcfe5f40e7403267b7c
Arnold Sujan Katru <96995931+ASKHOPE@users.noreply.github.com>
2024-10-19 01:36:14 +0530

    completed local

--- routes/class.js
+++ routes/class.js
@@ -7,18 +7,18 @@ const validation = require("../middleware/validator.js");
 const { isAuthenticated } = require('../middleware/authenticate');
 
 //C
-// routes.post('/',isAuthenticated, validation.classCreateValidationRules(), validation.validate, classController.createClass);
+routes.post('/',isAuthenticated, validation.classCreateValidationRules(), validation.validate, classController.createClass);
 
 //R
 routes.get('/', isAuthenticated,classController.getAll);
 routes.get('/:id',isAuthenticated, classController.getOne);
-// routes.get('/subject/:subject',isAuthenticated, validation.classBySubjectValidationRules(), validation.validate, classController.getSubject());
+routes.get('/subject/:subject',isAuthenticated, validation.classBySubjectValidationRules(), validation.validate, classController.getSubject());
 
 //U
-// routes.put('/:classId',isAuthenticated, validation.classUpdateValidationRules(), validation.validate, classController.updateClass);
+routes.put('/:classId',isAuthenticated, validation.classUpdateValidationRules(), validation.validate, classController.updateClass);
 
 //D
-// routes.delete('/:classId',isAuthenticated, validation.classDeleteByIdValidationRules(), validation.validate, classController.deleteClass);
+routes.delete('/:classId',isAuthenticated, validation.classDeleteByIdValidationRules(), validation.validate, classController.deleteClass);
 
 
 module.exports = routes;
