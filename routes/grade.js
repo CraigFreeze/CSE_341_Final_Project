9eb8b61945a0aeadb8033f37922df3d3b440a987
Arnold Sujan Katru <96995931+ASKHOPE@users.noreply.github.com>
2024-10-19 23:21:16 +0530

    update routes crud auth

--- routes/grade.js
+++ routes/grade.js
@@ -1,19 +1,26 @@
-const express = require('express');
-const routes = express.Router();
-
+//Dependencies
+const routes = require('express').Router();
 const gradeController = require('../controllers/gradeController.js')
 
+
+//Validation and Authentication
 const validation = require("../middleware/validator.js");
+const { isAuthenticated } = require('../middleware/authenticate');
 
+//C
+routes.post('/', isAuthenticated, validation.gradeCreateValidationRules(), validation.validate,  gradeController.createGrade)
 
-routes.get('/', gradeController.getAll());
-routes.get('/student/:studentId', validation.gradeFindByStudentIdValidationRules(), validation.validate, gradeController.getStudentId());
-routes.get('/:grade', validation.gradeFindByIdValidationRules(), validation.validate, gradeController.getGradeId());
-
-routes.post('/',validation.gradeCreateValidationRules(), validation.validate,  gradeController.createGrade)
+//R
 // routes.get('/', gradeController.getOne(''))
-routes.put('/:id', validation.gradeUpdateValidationRules(), validation.validate, gradeController.updateGrade)
-routes.delete('/:id', validation.gradeDeleteByIdValidationRules(), validation.validate, gradeController.deleteGrade)
+routes.get('/',isAuthenticated, gradeController.getAll());
+routes.get('/student/:studentId',isAuthenticated, validation.gradeFindByStudentIdValidationRules(), validation.validate, gradeController.getStudentId());
+routes.get('/:grade',isAuthenticated, validation.gradeFindByIdValidationRules(), validation.validate, gradeController.getGradeId());
+
+//U
+routes.put('/:id', isAuthenticated, validation.gradeUpdateValidationRules(), validation.validate, gradeController.updateGrade)
+
+//D
+routes.delete('/:id', isAuthenticated, validation.gradeDeleteByIdValidationRules(), validation.validate, gradeController.deleteGrade)
 
 
 module.exports = routes;
--- routes/student.js
+++ routes/student.js
@@ -1,19 +1,26 @@
+//Dependencies
 const routes = require('express').Router();
-
-const student = require('../controllers/studentController.js');
 const studentController = require('../controllers/studentController.js')
+const student = require('../controllers/studentController.js');
 
+//Validation and Authentication
 const validation = require("../middleware/validator.js");
+const { isAuthenticated } = require('../middleware/authenticate');
 
+//C
+routes.post('/',isAuthenticated, validation.studentCreateValidationRules(), validation.validate, studentController.createStudent)
 
-routes.get('/', studentController.getAll())
-routes.get('/:id', validation.studentFindByIdValidationRules(), validation.validate, studentController.getOne())
-routes.get('/findByFirstName/:first_name', validation.studentFindFirstNameValidationRules(), validation.validate, studentController.findByFirstName())
-routes.get('/findByLastName/:last_name', validation.studentFindLastNameValidationRules(), validation.validate, studentController.findByLastName())
-routes.post('/', validation.studentCreateValidationRules(), validation.validate, studentController.createStudent)
+//R
+routes.get('/',isAuthenticated, studentController.getAll())
+routes.get('/:id',isAuthenticated, validation.studentFindByIdValidationRules(), validation.validate, studentController.getOne())
+routes.get('/findByFirstName/:first_name',isAuthenticated, validation.studentFindFirstNameValidationRules(), validation.validate, studentController.findByFirstName())
+routes.get('/findByLastName/:last_name',isAuthenticated, validation.studentFindLastNameValidationRules(), validation.validate, studentController.findByLastName())
 
-routes.put('/:studentId', validation.studentUpdateValidationRules(), validation.validate, studentController.updateStudent)
-routes.delete('/:studentId', validation.studentDeleteByIdValidationRules(), validation.validate, studentController.deleteStudent)
+//U
+routes.put('/:studentId',isAuthenticated, validation.studentUpdateValidationRules(), validation.validate, studentController.updateStudent)
+
+//D
+routes.delete('/:studentId',isAuthenticated, validation.studentDeleteByIdValidationRules(), validation.validate, studentController.deleteStudent)
 
 
 module.exports = routes;
