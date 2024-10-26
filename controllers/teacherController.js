f95ce0967197fde4beff34389c71da094138dab8
Arnold Sujan Katru <96995931+ASKHOPE@users.noreply.github.com>
2024-10-27 04:34:18 +0530

    fixed everything

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -1,59 +1,79 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
+// Corrected getAll method
 const getAll = () => {
     //#swagger.tags=['Teacher']
     return async (req, res) => {
         const db = mongodb.getDb();
-        const result = await db.collection('teacher').find();
-        result.toArray().then((contacts) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(contacts);
-        });
-    }
-}
-
-const getOne = () => {
-    //#swagger.tags=['Teacher']
-    return async (req, res) => {
-        const id = new ObjectId(req.params.id)
-        const db = mongodb.getDb();
-        const result = await db.collection('teacher').find({ _id: id });
-        result.toArray().then((contacts) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(contacts[0]);
-        });
-    }
-}
-
-const getByName = () => {
-    //#swagger.tags=['Teacher']
-    try {
-        return async (req, res) => {
-            const firstName = req.params.name.toLowerCase();
-            const db = mongodb.getDb();
+        try {
             const result = await db.collection('teacher').find();
             result.toArray().then((contacts) => {
                 res.setHeader('Content-Type', 'application/json');
-                var found = [];
-                contacts.map((contact) => { if (contact.first_name.toLowerCase() == firstName) found.push(contact) });
-                if (found.length > 0) {
-                    res.status(200).json(found);
+                if (contacts.length > 0) {
+                    res.status(200).json(contacts);
                 } else {
-                    res.status(200).send('No teachers were found');
+                    res.status(404).json({ message: "No teachers found in the database" });
                 }
             });
+        } catch (error) {
+            res.status(500).json({ message: "An error occurred while retrieving teachers", error: error.message });
         }
-    } catch {
-        return res.status(500).send('Some error occurred while tring to find teacher by first name');
-    }
-}
+    };
+};
+
+// Corrected getOne method
+const getOne = () => {
+    //#swagger.tags=['Teacher']
+    return async (req, res) => {
+        const id = new ObjectId(req.params.id);
+        const db = mongodb.getDb();
+
+        try {
+            const result = await db.collection('teacher').find({ _id: id });
+            result.toArray().then((teachers) => {
+                if (teachers.length > 0) {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status(200).json(teachers[0]);
+                } else {
+                    res.status(404).json({ message: "Teacher not found" });
+                }
+            });
+        } catch (error) {
+            res.status(500).json({ message: "An error occurred while retrieving the teacher", error: error.message });
+        }
+    };
+};
+
+const getByName = () => {
+    //#swagger.tags=['Teacher']
+    return async (req, res) => {
+        try {
+            const firstName = req.params.name?.toLowerCase();
+            const db = mongodb.getDb();
+            const result = await db.collection('teacher').find();
+            const contacts = await result.toArray();
+
+            res.setHeader('Content-Type', 'application/json');
+            const found = contacts.filter((contact) => contact.first_name?.toLowerCase() === firstName);
+
+            if (found.length > 0) {
+                return res.status(200).json(found);
+            } else {
+                return res.status(404).json({ message: "No teachers were found with the specified first name" });
+            }
+        } catch (error) {
+            return res.status(500).json({ message: "An error occurred while trying to find teacher by first name", error: error.message });
+        }
+    };
+};
 
 
-const createTeacher = async (req, res, next) => {
+// Corrected createTeacher method
+const createTeacher = async (req, res) => {
     //#swagger.tags=['Teacher']
     if (!req.body) {
-        res.status(400).send();
+        res.status(400).json({ message: "No teacher data provided" });
         return;
     }
     const newTeacher = {
@@ -61,50 +81,72 @@ const createTeacher = async (req, res, next) => {
         last_name: req.body.last_name,
         subject: req.body.subject,
         classes: req.body.classes,
-    }
+    };
     const db = mongodb.getDb();
-    const result = await db.collection('teacher').insertOne(newTeacher);
-    if (response.acknowledged) {
-        res.status(201).json(response);
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while creating Teacher');
-    }
-}
 
-const updateTeacher = async (req, res, next) => {
+    try {
+        const result = await db.collection('teacher').insertOne(newTeacher);
+        if (result.acknowledged) {
+            res.status(201).json(result);
+        } else {
+            res.status(500).json({ message: "An error occurred while creating the teacher" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred while creating the teacher", error: error.message });
+    }
+};
+
+// Corrected updateTeacher method
+const updateTeacher = async (req, res) => {
     //#swagger.tags=['Teacher']
     const teacherId = new ObjectId(req.params.id);
-    const updatedteacher = {
+    if (!req.body) {
+        res.status(400).json({ message: "Data to update cannot be empty" });
+        return;
+    }
+    const updatedTeacher = {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         subject: req.body.subject,
         classes: req.body.classes,
     };
     const db = mongodb.getDb();
-    const result = await db.collection('teacher').replaceOne({ _id: teacherId }, updatedteacher);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occured while updating the teacher');
+
+    try {
+        const result = await db.collection('teacher').replaceOne({ _id: teacherId }, updatedTeacher);
+        if (result.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ message: `Teacher with ID ${teacherId} not found` });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred while updating the teacher", error: error.message });
     }
 };
 
-const deleteTeacher = async (req, res, next) => {
+// Corrected deleteTeacher method
+const deleteTeacher = async (req, res) => {
     //#swagger.tags=['Teacher']
     const teacherId = new ObjectId(req.params.id);
     const db = mongodb.getDb();
-    const result = await db.collection('teacher').deleteOne({ _id: teacherId });
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occured while deleting the teacher');
-    }
-}
 
+    try {
+        const result = await db.collection('teacher').deleteOne({ _id: teacherId });
+        if (result.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ message: `Teacher with ID ${teacherId} not found` });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred while deleting the teacher", error: error.message });
+    }
+};
 
 module.exports = {
-
-
-    getOne, getAll, createTeacher, getByName, updateTeacher, deleteTeacher
-
-}
+    getAll,
+    getOne,
+    getByName,
+    createTeacher,
+    updateTeacher,
+    deleteTeacher,
+};
--- middleware/validator.js
+++ middleware/validator.js
@@ -1,57 +1,57 @@
 const { check, query, param, validationResult } = require("express-validator");
 const validation = {};
 
-
 /*#######################################
 STUDENT VALIDATION RULES
 #######################################*/
 
 validation.studentFindByIdValidationRules = () => {
     return [
-        param("id").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId")
+        param("id").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)")
     ];
-}
+};
 
 validation.studentFindLastNameValidationRules = () => {
     return [
         param("last_name").isString().isLength({ min: 1, max: 40 }).withMessage("Last name must be a string with 1 to 40 characters")
-    ]
-}
+    ];
+};
 
 validation.studentFindFirstNameValidationRules = () => {
     return [
         param("first_name").isString().isLength({ min: 1, max: 40 }).withMessage("First name must be a string with 1 to 40 characters")
-    ]
-}
+    ];
+};
 
 validation.studentCreateValidationRules = () => {
     return [
         check("first_name").isString().isLength({ min: 1, max: 40 }).withMessage("First name must be a string with 1 to 40 characters"),
-        check("last_name").isString().isLength({min: 1, max: 40}).withMessage("Last name must be a string with 1 to 40 characters"),
+        check("last_name").isString().isLength({ min: 1, max: 40 }).withMessage("Last name must be a string with 1 to 40 characters"),
         check("enrolled_classes").isArray().withMessage("Enrolled classes must be an array"),
         check("date_of_birth").isDate({ format: 'YYYY-MM-DD' }).withMessage("Date of birth must be in the format YYYY-MM-DD"),
-        check("address").isString().isLength({min: 1, max: 120}).withMessage("Address must be a string with 1 to 120 characters"),
+        check("address").isString().isLength({ min: 1, max: 120 }).withMessage("Address must be a string with 1 to 120 characters"),
         check("grade_level").isInt({ min: 1, max: 12 }).withMessage("Grade level must be an integer between 1 and 12")
-    ]
-}
+    ];
+};
 
 validation.studentUpdateValidationRules = () => {
     return [
-        param("studentId").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId"),
+        param("studentId").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)"),
         check("first_name").isString().isLength({ min: 1, max: 40 }).withMessage("First name must be a string with 1 to 40 characters"),
-        check("last_name").isString().isLength({min: 1, max: 40}).withMessage("Last name must be a string with 1 to 40 characters"),
+        check("last_name").isString().isLength({ min: 1, max: 40 }).withMessage("Last name must be a string with 1 to 40 characters"),
         check("enrolled_classes").isArray().withMessage("Enrolled classes must be an array"),
         check("date_of_birth").isDate({ format: 'YYYY-MM-DD' }).withMessage("Date of birth must be in the format YYYY-MM-DD"),
-        check("address").isString().isLength({min: 1, max: 120}).withMessage("Address must be a string with 1 to 120 characters"),
+        check("address").isString().isLength({ min: 1, max: 120 }).withMessage("Address must be a string with 1 to 120 characters"),
         check("grade_level").isInt({ min: 1, max: 12 }).withMessage("Grade level must be an integer between 1 and 12")
-    ]
-}
+    ];
+};
 
 validation.studentDeleteByIdValidationRules = () => {
     return [
+        // param("id").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)")
         param("id").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId")
     ];
-}
+};
 
 /*#######################################
 CLASS VALIDATION RULES
@@ -59,40 +59,41 @@ CLASS VALIDATION RULES
 
 validation.classFindByIdValidationRules = () => {
     return [
-        param("id").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId")
+        param("id").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)")
     ];
-}
+};
 
 validation.classCreateValidationRules = () => {
     return [
         check("course_code").isString().isLength({ min: 1, max: 40 }).withMessage("Course code must be a string with 1 to 40 characters"),
-        check("subject").isString().isLength({min: 1, max: 40}).withMessage("subject must be a string with 1 to 40 characters"),
-        check("class_description").isString().isLength({min: 1, max: 120}).withMessage("Class ddescription must be a string with 1 to 120 characters"),
+        check("subject").isString().isLength({ min: 1, max: 40 }).withMessage("Subject must be a string with 1 to 40 characters"),
+        check("class_description").isString().isLength({ min: 1, max: 120 }).withMessage("Class description must be a string with 1 to 120 characters"),
         check("max_class_size").isInt({ min: 1, max: 1000 }).withMessage("Max class size must be an integer between 1 and 1000")
-    ]
-}
+    ];
+};
 
 validation.classBySubjectValidationRules = () => {
     return [
-        check("subject").isString().isLength({min: 1, max: 40}).withMessage("Subject must be a string with 1 to 40 characters")
-    ]
-}
+        check("subject").isString().isLength({ min: 1, max: 40 }).withMessage("Subject must be a string with 1 to 40 characters")
+    ];
+};
 
 validation.classUpdateValidationRules = () => {
     return [
-        param("classId").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId"),
+        param("classId").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)"),
         check("course_code").isString().isLength({ min: 1, max: 40 }).withMessage("Course code must be a string with 1 to 40 characters"),
-        check("subject").isString().isLength({min: 1, max: 40}).withMessage("subject must be a string with 1 to 40 characters"),
-        check("class_description").isString().isLength({min: 1, max: 120}).withMessage("Class description must be a string with 1 to 120 characters"),
+        check("subject").isString().isLength({ min: 1, max: 40 }).withMessage("Subject must be a string with 1 to 40 characters"),
+        check("class_description").isString().isLength({ min: 1, max: 120 }).withMessage("Class description must be a string with 1 to 120 characters"),
         check("max_class_size").isInt({ min: 1, max: 1000 }).withMessage("Max class size must be an integer between 1 and 1000")
-    ]
-}
+    ];
+};
 
 validation.classDeleteByIdValidationRules = () => {
     return [
-        param("classId").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId")
+        // param("classId").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)")
+        param("id").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId")
     ];
-}
+};
 
 /*#######################################
 GRADE VALIDATION RULES
@@ -100,39 +101,39 @@ GRADE VALIDATION RULES
 
 validation.gradeFindByIdValidationRules = () => {
     return [
-        param("grade").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId")
+        param("grade").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)")
     ];
-}
+};
 
 validation.gradeFindByStudentIdValidationRules = () => {
     return [
-        param("studentId").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId")
+        param("studentId").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)")
     ];
-}
+};
 
 validation.gradeCreateValidationRules = () => {
     return [
-        check("student_id").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId"),
+        check("student_id").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)"),
         check("assignment_name").isString().isLength({ min: 1, max: 40 }).withMessage("Assignment name must be a string with 1 to 40 characters"),
-        check("grade").isString().isLength({min: 1, max: 2}).withMessage("Grade must be a string with 1 to 2 characters")
-    ]
-}
+        check("grade").isString().isLength({ min: 1, max: 2 }).withMessage("Grade must be a string with 1 to 2 characters")
+    ];
+};
 
 validation.gradeUpdateValidationRules = () => {
     return [
-        param("id").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId"),
-        check("student_id").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId"),
+        param("id").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)"),
+        check("student_id").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)"),
         check("assignment_name").isString().isLength({ min: 1, max: 40 }).withMessage("Assignment name must be a string with 1 to 40 characters"),
-        check("grade").isString().isLength({min: 1, max: 2}).withMessage("Grade must be a string with 1 to 2 characters")
-    ]
-}
+        check("grade").isString().isLength({ min: 1, max: 2 }).withMessage("Grade must be a string with 1 to 2 characters")
+    ];
+};
 
 validation.gradeDeleteByIdValidationRules = () => {
     return [
-        param("classId").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId")
+        // param("classId").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)")
+        param("id").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId")
     ];
-}
-
+};
 
 /*#######################################
 TEACHER VALIDATION RULES
@@ -140,59 +141,58 @@ TEACHER VALIDATION RULES
 
 validation.teacherFindByIdValidationRules = () => {
     return [
-        param("id").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId")
+        param("id").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)")
     ];
-}
+};
 
 validation.teacherFindNameValidationRules = () => {
     return [
-        param("name").isString().isLength({ min: 1, max: 40 }).withMessage("Last name must be a string with 1 to 40 characters")
-    ]
-}
+        param("name").isString().isLength({ min: 1, max: 40 }).withMessage("Name must be a string with 1 to 40 characters")
+    ];
+};
 
 validation.teacherCreateValidationRules = () => {
     return [
         check("first_name").isString().isLength({ min: 1, max: 40 }).withMessage("First name must be a string with 1 to 40 characters"),
-        check("last_name").isString().isLength({min: 1, max: 40}).withMessage("Last name must be a string with 1 to 40 characters"),
-        check("subject").isString().isLength({min: 1, max: 40}).withMessage("Subject must be a string with 1 to 40 characters"),
-        check("classes").isArray().withMessage("classes must be an array")
-    ]
-}
+        check("last_name").isString().isLength({ min: 1, max: 40 }).withMessage("Last name must be a string with 1 to 40 characters"),
+        check("subject").isString().isLength({ min: 1, max: 40 }).withMessage("Subject must be a string with 1 to 40 characters"),
+        check("classes").isArray().withMessage("Classes must be an array")
+    ];
+};
 
 validation.teacherUpdateValidationRules = () => {
     return [
-        param("id").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId"),
+        param("id").matches(/^[0-9a-fA-F]{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)"),
         check("first_name").isString().isLength({ min: 1, max: 40 }).withMessage("First name must be a string with 1 to 40 characters"),
-        check("last_name").isString().isLength({min: 1, max: 40}).withMessage("Last name must be a string with 1 to 40 characters"),
-        check("subject").isString().isLength({min: 1, max: 40}).withMessage("Subject must be a string with 1 to 40 characters"),
-        check("classes").isArray().withMessage("classes must be an array")
-    ]
-}
+        check("last_name").isString().isLength({ min: 1, max: 40 }).withMessage("Last name must be a string with 1 to 40 characters"),
+        check("subject").isString().isLength({ min: 1, max: 40 }).withMessage("Subject must be a string with 1 to 40 characters"),
+        check("classes").isArray().withMessage("Classes must be an array")
+    ];
+};
 
 validation.teacherDeleteByIdValidationRules = () => {
     return [
+        // param("id").matches(/^{24}$/).withMessage("Invalid ID format. Must be a 24-character hex string (MongoDB ObjectId)")
         param("id").isMongoId().withMessage("Invalid ID format. Must be a valid MongoDB ObjectId")
     ];
-}
-
+};
 
 /*#######################################
-VALDATION PROCESS
+VALIDATION PROCESS
 #######################################*/
 
 validation.validate = (req, res, next) => {
-    const errors = validationResult(req)
+    const errors = validationResult(req);
     if (errors.isEmpty()) {
-        return next()
+        return next();
     }
 
-    console.log(errors);
-    const extractedErrors = []
-    errors.array().map(err => extractedErrors.push({ [err.path]: err.msg })) // ! If not path try param
+    const extractedErrors = [];
+    errors.array().map(err => extractedErrors.push({ [err.path]: err.msg }));
 
     return res.status(422).json({
         errors: extractedErrors,
-    })
-}
+    });
+};
 
 module.exports = validation;
